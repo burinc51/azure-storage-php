3ed4a496fdf879137a76c5a881c924b5428b7b8d
gotzila64 <47980157+burinc51@users.noreply.github.com>
2023-10-15 15:27:29 +0700

    Update download.php

--- download.php
+++ download.php
@@ -10,12 +10,13 @@
     $blobClient = BlobRestProxy::createBlobService($connectionString);
 
     $blobName = urldecode($_GET["name"]);
-    try {
-        $url = "https://burinstorage.blob.core.windows.net/mybob/".$blobName;
-        header("Location: $url");
-    } catch (ServiceException $e) {
-        echo '<script>alert("'.$e->getMessage().'");</script>';
-    }
+    echo $blobName;
+    // try {
+    //     $url = "https://burinstorage.blob.core.windows.net/mybob/".$blobName;
+    //     header("Location: $url");
+    // } catch (ServiceException $e) {
+    //     echo '<script>alert("'.$e->getMessage().'");</script>';
+    // }
 ?>
 
 
